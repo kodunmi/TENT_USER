1b4a835485f1ac8b28fb90a1dfc56bea8eb18d71
kodunmi <53086177+kodunmi@users.noreply.github.com>
2024-03-15 14:44:40 +0100

    added build part

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-const path = require('path')
-const withImages = require('next-images');
-
+const path = require("path");
+const withImages = require("next-images");
 
 module.exports = withImages();
 module.exports = {
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-    },
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  distDir: "build",
+};
